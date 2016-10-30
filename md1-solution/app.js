b700850e0c9382e56f329a0e1843cdbd5296662f
Mateusz M <Mateusz M>
2016-10-30 20:10:51 +0100

    'solution'

--- md1-solution/app.js
+++ md1-solution/app.js
@@ -1,9 +1,39 @@
-/**
- * Created by 8570w on 2016-10-29.
- */
 (function(){
     'use strict';
 
     angular.module('LunchCheck',[])
+        .controller('LunchController', LaunchController);
+
+    LaunchController.$inject=['$scope'];
+    function LaunchController($scope){
+        var foodCount = 0;
+
+        $scope.alert = '';
+        $scope.food = '';
+        $scope.checkFood = function(){
+            foodCount = countFood($scope.food);
+
+            if (foodCount <= 3 && foodCount >0){
+                $scope.alert = 'Enjoy!';
+            }
+            else if(foodCount === 0){
+                $scope.alert = 'Please enter data first!';
+            }
+            else{
+                $scope.alert ='Too much!';
+            }
+        };
+    }
+    function countFood(food){
+        var count =0;
+        var temp_words = food.split(",");
+        temp_words.forEach(function(word,index){
+            // console.log(index);
+            if (word.length != 0){
+                count+=1;
+            }
+        });
+        return count;
+    }
 
 })();
